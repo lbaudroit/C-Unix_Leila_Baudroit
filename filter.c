e3291028cbaaa244c4f8ebaba552368334963688
lbaudroit <109075845+lbaudroit@users.noreply.github.com>
2025-11-19 19:48:22 +0100

    feat: exercice 3.1 (lecture du PPM)

---
+++ filter.c
@@ -0,0 +1,73 @@
+#include "image.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // Vérification des arguments
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <input_file> <output_file> <filter_type>\n", argv[0]);
+        fprintf(stderr, "Vous devez fournir des noms de fichiers d'entrée et de sortie et un type de filtre. Abandon\n");
+        return -1;
+    }
+
+    const char *inputFilename = argv[1];
+    const char *outputFilename = argv[2];
+    const char *filterType = argv[3];
+
+    Image *source = image_read_txt(inputFilename);
+    if (source == NULL) {
+        fprintf(stderr, "Erreur lecture fichier d'entrée\n");
+        return -1;
+    }
+
+    int h = source->height;
+    int w = source->width;
+    Image *destination = image_create(w, h);
+
+    if (destination == NULL) {
+        image_free(source);
+        return -1;
+    }
+
+    // Application du filtre pixel par pixel
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            unsigned char rouge = image_get_red(source, x, y);
+            unsigned char vert = image_get_green(source, x, y);
+            unsigned char bleu = image_get_blue(source, x, y);
+
+            if (strcmp(filterType, "copy") == 0) {
+                image_set_pixel(destination, x, y, rouge, vert, bleu);
+            }
+            else if (strcmp(filterType, "dark") == 0) {
+                image_set_pixel(destination, x, y, rouge / 2, vert / 2, bleu / 2);
+            }
+            else if (strcmp(filterType, "bright") == 0) {
+                unsigned char r = (rouge > 128) ? 255 : rouge * 2;
+                unsigned char g = (vert > 128) ? 255 : vert * 2;
+                unsigned char b = (bleu > 128) ? 255 : bleu * 2;
+                image_set_pixel(destination, x, y, r, g, b);
+            }
+            else if (strcmp(filterType, "grayscale") == 0) {
+                unsigned char gray = (unsigned char)(0.299 * rouge + 0.587 * vert + 0.114 * bleu);
+                image_set_pixel(destination, x, y, gray, gray, gray);
+            }
+            else {
+                fprintf(stderr, "Type de filtre inconnu: %s\n", filterType);
+                image_free(source);
+                image_free(destination);
+                return -1;
+            }
+        }
+    }
+
+    // Sauvegarde de l'image filtrée
+    image_save_txt(destination, outputFilename);
+    printf("Image filtrée sauvegardée dans %s\n", outputFilename);
+
+    image_free(source);
+    image_free(destination);
+
+    return 0;
+}
--- image.c
+++ image.c
@@ -19,10 +19,8 @@ void image_free(Image *img) {
 }
 
 void image_set_pixel(Image *img, int x, int y, unsigned char r, unsigned char g, unsigned char b) {
-	printf("%d %d %d %d %d\n", img != NULL, x >= 0, x < img -> width, y >= 0, y < img -> height);
-    if (img != NULL && x >= 0 && x < img -> width && y >= 0 && y < img -> height) {
+	if (img != NULL && x >= 0 && x < img -> width && y >= 0 && y < img -> height) {
         int index = ( y * img -> width + x ) * 3;
-		printf("Ajouté à l'index %d\n", index);
         img -> pixels[index + 0] = r;
         img -> pixels[index + 1] = g;
         img -> pixels[index + 2] = b;
@@ -58,3 +56,69 @@ void image_save_txt(Image *img, const char *filename) {
     fclose(fp); // Fermeture du fichier
     printf("Image PPM créée avec succès !\n");
 }
+
+unsigned char image_get_red(Image *img, int x, int y) {
+	int index = ( y * img -> width + x ) * 3;
+	return img -> pixels[index];
+}
+
+unsigned char image_get_green(Image *img, int x, int y) {
+	int index = ( y * img -> width + x ) * 3;
+	return img -> pixels[index + 1];
+}
+
+unsigned char image_get_blue(Image *img, int x, int y) {
+	int index = ( y * img -> width + x ) * 3;
+	return img -> pixels[index + 2];
+
+}
+
+Image* image_read_txt(const char *filename) {
+	FILE *fp = fopen(filename, "r");
+    Image *img = 0;
+
+    if (fp == NULL) {
+        perror("Erreur d'ouverture du fichier");
+    }
+
+	int line_no = 1;
+    int was_read = 0;
+    int y = 0;
+    int width, height;
+
+	do {
+        int buffer_size = 0;
+	    char *line = NULL;
+		was_read = getline(&line, &buffer_size, fp);
+
+        /* Création de l'image avec sa longueur et largeur */
+        if (line_no == 2) {
+            sscanf(line, "%d %d", &width, &height);
+            img = image_create(width, height);
+        }
+
+        /* Valeurs des pixels */
+        if (line_no > 3) {
+            int x = 0;
+            unsigned char value, r, g, b = 0;
+
+            int offset = 0;
+            char* char_ptr = line;
+
+            while (sscanf(char_ptr, "%hhd %hhd %hhd %n", &r, &g, &b, &offset) == 3) {
+                image_set_pixel(img, x, y, r, g, b);
+                char_ptr += offset;
+                x++;
+            }
+            y++;
+        }
+
+		line_no++;
+        free(line);
+	} while (was_read != -1);
+
+    fclose(fp);
+
+    return img;
+}
+
